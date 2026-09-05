5e44899877650c7d9d2c671113c9538a8b5757db
Chirasak Tapphae <dearchirasak@gmail.com>
2026-09-05 16:07:59 +0700

    feat: initialize product catalog UI with Tailwind CSS, DaisyUI, and ProductCard component

--- Zeta-Jersey-Store/src/App.jsx
+++ Zeta-Jersey-Store/src/App.jsx
@@ -5,17 +5,11 @@ import productData from "./data/products.json";
 const App = () => {
   const [products, setProducts] = useState(productData);
 
-  const [selectedProduct, setSelectProduct] = useState(null);
-
   return (
     <div>
       <div>
         {products.map((item) => (
-          <ProductCard
-            key={item.id}
-            product={item}
-            onSelect={() => setSelectProduct(item)}
-          />
+          <ProductCard key={item.id} product={item} />
         ))}
       </div>
     </div>
--- Zeta-Jersey-Store/src/components/ProductCard.jsx
+++ Zeta-Jersey-Store/src/components/ProductCard.jsx
@@ -1,14 +1,20 @@
-import { ShoppingCart } from "lucide-react";
+import { Heart, ShoppingCart } from "lucide-react";
 
-const ProductCard = ({ product, onSelect }) => {
+const ProductCard = ({ product }) => {
   return (
     <div className="card w-96 rounded-xl bg-yellow-100">
       <figure className="px-5 pt-5">
         <img className="rounded-xl" src={product.imageUrl} alt={product.name} />
+        <span className="badge badge-outline absolute top-7 left-7 rounded-xl border-0 font-medium bg-zeta-sub-lighter text-zeta-sub-dark">
+          NEW
+        </span>
+        <button className="absolute top-7 right-7 bg-white rounded-full p-1">
+          <Heart size={20} />
+        </button>
       </figure>
       <div className="card-body">
         <h2 className="card-title text-2xl font-bold ">{product.name}</h2>
-        <div className="badge badge-outline rounded-xl border-0 bg-zeta-main-lighter  text-zeta-main">
+        <div className="badge badge-outline rounded-xl border-0 bg-zeta-main-lighter text-zeta-main">
           {product.team}
         </div>
         <p>{product.description}</p>
@@ -21,16 +27,14 @@ const ProductCard = ({ product, onSelect }) => {
             <div className="flex justify-between text-xl font-bold">
               <span className="">Price</span>
               <span className="text-zeta-main">
+                <span className="text-sm font-medium mr-0.5">฿</span>
                 {product.price.toLocaleString()}
                 {/* convert number to string with format */}
               </span>
             </div>
           </div>
 
-          <button
-            onClick={onSelect}
-            className="btn w-full bg-zeta-main text-white rounded-4xl "
-          >
+          <button className="btn w-full bg-zeta-main text-white rounded-4xl ">
             <span>
               <ShoppingCart size={20} />
             </span>
