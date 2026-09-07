f0b05b7bf2ece9e0e7edb2d88981692c50a837b9
Ajjima <ajjimajom@gmail.com>
2026-09-07 23:21:51 +0700

    feat: implement homepage UI with Collections and LeagueCard components

--- Zeta-Jersey-Store/src/App.jsx
+++ Zeta-Jersey-Store/src/App.jsx
@@ -1,4 +1,27 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import PromoBar from "./components/PromoBar";
+import LeagueCard from "./components/LeagueCard";
+import Collections from "./components/Collections";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar page="home" />
+      <Navbar page="other" />
+
+      <div>
+        <PromoBar />
+      </div>
+
+      <LeagueCard />
+      <Collections />
+
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
 };
+
 export default App;
