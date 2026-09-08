32a605d90a7437c6e12d94918d235768187269c7
Chirasak Tapphae <dearchirasak@gmail.com>
2026-09-08 08:57:17 +0700

    feat: initialize App component with base layout and product detail view

--- Zeta-Jersey-Store/src/App.jsx
+++ Zeta-Jersey-Store/src/App.jsx
@@ -3,31 +3,18 @@ import Footer from "./components/Footer";
 import PromoBar from "./components/PromoBar";
 import LeagueCard from "./components/LeagueCard";
 import Collections from "./components/Collections";
-
-const App = () => {
-  return (
-    <div>
-      <Navbar page="home" />
-      <Navbar page="other" />
-
-      <div>
-        <PromoBar />
-      </div>
-
-      <LeagueCard />
-      <Collections />
-
-      <div>
-        <Footer />
 import ProductDetail from "./components/ProductDetail";
 import Suggestion from "./components/Suggestion";
 const App = () => {
   return (
     <div>
-      <div>
-        <ProductDetail />
-        <Suggestion />
-      </div>
+      <Navbar page="other" />
+      <PromoBar />
+      <ProductDetail />
+      <Suggestion />
+      {/* <LeagueCard />
+      <Collections /> */}
+      <Footer />
     </div>
   );
 };
