a40c311499125a18922da31007e871c192e011ca
Chirasak Tapphae <dearchirasak@gmail.com>
2026-09-17 13:39:02 +0700

    feat: create App.jsx component

--- Zeta-Jersey-Store/src/App.jsx
+++ Zeta-Jersey-Store/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import ProductDetailPage from "./pages/ProductDetailPage";
+import CheckoutPage from "./pages/CheckoutPage";
 const App = () => {
   return (
     <div>
@@ -8,6 +9,7 @@ const App = () => {
         <Route path="/home" element={<LandingPage />} />
         {/* หน้ารายละเอียดสินค้า: :id คือตัวแปร Dynamic */}
         <Route path="/products/:id" element={<ProductDetailPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
       </Routes>
     </div>
   );
