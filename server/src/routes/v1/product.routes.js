4a4d64f4e2dcd01c1f251f5bd6c109c7f585d4b5
Chirasak Tapphae <dearchirasak@gmail.com>
2026-09-17 09:38:24 +0700

    feat: add product routes for getting all products and product by id

--- server/src/routes/v1/product.routes.js
+++ server/src/routes/v1/product.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { getProducts } from "../../controllers/product.controller.js";
-import { getProductById } from "../../controllers/product.controller.js";
+import {
+  getProducts,
+  getProductById,
+} from "../../controllers/product.controller.js";
 export const router = Router();
 
 // Read all product
