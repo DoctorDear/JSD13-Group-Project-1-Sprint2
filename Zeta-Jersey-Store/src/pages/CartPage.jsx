b4c0cb27192737d27ca5285f9f387662bf93b1ea
Supakorn <supakornprapis@gmail.com>
2026-09-17 22:23:12 +0700

    edit responsive and components

--- Zeta-Jersey-Store/src/pages/CartPage.jsx
+++ Zeta-Jersey-Store/src/pages/CartPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import CartItemCard from '../components/CartItemCard';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
