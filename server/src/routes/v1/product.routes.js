16ab77733ef975502b4bd0a83f5b3291c7e5c1f4
Chirasak Tapphae <dearchirasak@gmail.com>
2026-09-17 08:37:47 +0700

    feat: add product controller and routes for fetching and filtering products

--- server/src/routes/v1/product.routes.js
+++ server/src/routes/v1/product.routes.js
@@ -1,37 +1,10 @@
 import { Router } from "express";
-import { Product } from "../../models/Product.model.js";
-
+import { getProducts } from "../../controllers/product.controller.js";
+import { getProductById } from "../../controllers/product.controller.js";
 export const router = Router();
 
-// Read product
-router.get("/", async (req, res, next) => {
-  try {
-    const { search, ...filters } = req.query;
-    const query = {};
+// Read all product
+router.get("/", getProducts);
 
-    // 1. ถ้ามีคำค้นหา (Search คลุมทั้ง name, description, tag)
-    if (search) {
-      const searchRegex = { $regex: search.trim(), $options: "i" };
-      query.$or = [
-        { name: searchRegex },
-        { description: searchRegex },
-        { tag: searchRegex },
-      ];
-    }
-
-    // 2. ถ้ามี Filter อื่นๆ (วนลูปจัดการอัตโนมัติ)
-    const allowedFilters = ["category", "edition", "tag"];
-    allowedFilters.forEach((field) => {
-      if (filters[field]) {
-        const cleanVal = filters[field].replace(/-/g, " ").trim();
-        query[field] = { $regex: `^${cleanVal}$`, $options: "i" };
-      }
-    });
-
-    const products = await Product.find(query);
-
-    return res.status(200).json(products);
-  } catch (err) {
-    next(err);
-  }
-});
+// Read product by id
+router.get("/:id", getProductById);
